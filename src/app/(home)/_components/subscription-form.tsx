7f4dc616c1c942cb74406ae5d5a2f832ee318819
ezin1 <eziof.dev@gmail.com>
2025-02-19 09:38:22 -0300

    feat: integrate react-hook-form and zod for subscription form validation; update InviteLinkInput to accept inviteLink prop

--- src/app/(home)/_components/subscription-form.tsx
+++ src/app/(home)/_components/subscription-form.tsx
@@ -1,30 +1,73 @@
+"use client";
 import { Button } from "@/app/components/button";
 import { InputField, InputIcon, InputRoot } from "@/app/components/input";
 import { ArrowRight, Mail, User } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const subscriptionSchema = z.object({
+  name: z.string().min(2, "Digite seu nome completo"),
+  email: z.string().email("Digite um e-mail válido"),
+});
+
+type SubscriptionSchema = z.infer<typeof subscriptionSchema>;
 
 export function SubscriptionForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubscriptionSchema>({
+    resolver: zodResolver(subscriptionSchema),
+  });
+
+  function onSubscribe(data: SubscriptionSchema) {
+    console.log(data);
+  }
+
   return (
     <form
       className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
-      action=""
+      onSubmit={handleSubmit(onSubscribe)}
     >
       <h2 className="text-xl text-gray-200 font-heading font-semibold">
         Inscrição
       </h2>
       <div className="space-y-3">
-        <InputRoot>
-          <InputIcon>
-            <User />
-          </InputIcon>
-          <InputField type="text" placeholder="Nome completo" />
-        </InputRoot>
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <User />
+            </InputIcon>
+            <InputField
+              {...register("name")}
+              type="text"
+              placeholder="Nome completo"
+            />
+          </InputRoot>
 
-        <InputRoot>
-          <InputIcon>
-            <Mail />
-          </InputIcon>
-          <InputField type="email" placeholder="E-mail" />
-        </InputRoot>
+          {errors.name && (
+            <p className="text-danger text-xs font-semibold">{errors.name.message}</p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <Mail />
+            </InputIcon>
+            <InputField
+              {...register("email")}
+              type="email"
+              placeholder="E-mail"
+            />
+          </InputRoot>
+
+          {errors.email && (
+            <p className="text-danger text-xs font-semibold">{errors.email.message}</p>
+          )}
+        </div>
       </div>
 
       <Button type="submit">
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -5,6 +5,7 @@ import { Stats } from "./_components/stats";
 import { InviteLinkInput } from "./_components/invite-link-input";
 
 export default function Invite() {
+  const inviteLink = "https://github.com/ezin1"
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -31,7 +32,7 @@ export default function Invite() {
             </p>
           </div>
 
-          <InviteLinkInput />
+          <InviteLinkInput inviteLink={inviteLink}/>
 
           <Stats />
         </div>
